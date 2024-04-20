11e4cbdb4d925c659abcb6b62e4721d9189098c5
anyel-ec <paulangel826@gmail.com>
2024-04-19 19:57:34 -0500

    Refactor register screen and add custom form fields

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -30,7 +30,7 @@ class _RegisterView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               FlutterLogo(size: 100),
-
+              SizedBox(height: 20),
               _RegisterForm(), 
 
              
@@ -43,37 +43,103 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
 
   const _RegisterForm();
 
   @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
 
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Nombre de usuario',
-            prefixIcon: Icon(Icons.person),
-          ),
-          const SizedBox(height: 20),
-          const CustomTextFormField(
-            label: 'Correo electrónico',
-            prefixIcon: Icon(Icons.email),
+            prefixIcon: const Icon(Icons.person),
+            onChanged: (value) => username = value, 
+            validator: (value){
+              if (value == null){
+                return 'El nombre de usuario es requerido';
+              }
+              if(value.isEmpty){
+                return 'El nombre de usuario es requerido';
+              }
+              // debe de tener mas de 6 letras 
+              if(value.length < 6){
+                return 'El nombre de usuario debe de tener al menos 6 caracteres';
+              }
+              return null;
+            },
           ),
 
           const SizedBox(height: 20),
-          const CustomTextFormField(
+
+           CustomTextFormField(
+            label: 'Correo electrónico',
+            prefixIcon: const Icon(Icons.email),
+            onChanged: (value ) => email = value,
+            validator: (value){
+              if (value == null){
+                return 'El correo electrónico es requerido';
+              }
+              if(value.isEmpty){
+                return 'El correo electrónico es requerido';
+              }
+              if(!value.contains('@')){
+                return 'El correo electrónico no es válido';
+              }
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+
+              if(!emailRegExp.hasMatch(value)){
+                return 'El correo electrónico no es válido';
+              }
+              return null;
+            },
+          ),
+
+          const SizedBox(height: 20),
+           CustomTextFormField(
             label: 'Contraseña',
-            prefixIcon: Icon(Icons.lock),
+            prefixIcon: const Icon(Icons.lock),
             obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value){
+              if (value == null){
+                return 'La contraseña es requerida';
+              }
+              if(value.isEmpty){
+                return 'La contraseña es requerida';
+              }
+              if(value.length < 6){
+                return 'La contraseña debe de tener al menos 6 caracteres';
+              }
+              return null;
+            },
           ),
 
           const SizedBox(height: 20),
 
           FilledButton.tonalIcon(
-            onPressed: (){},
+            onPressed: (){
+              if(_formKey.currentState!.validate()){
+                print('username: $username');
+                print('email: $email');
+                print('password: $password');
+              }
+              
+            },
             label: const Text('Guardar'),
             icon: const Icon(Icons.save),
           )
